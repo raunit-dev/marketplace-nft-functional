ccdb05ecc4f5e4d8e3e3ebb02446dfa88d2af013
raunit45 <raunitjaiswal510@gmail.com>
2025-04-16 19:31:50 +0000

    imcomplete listing.rs

---
+++ programs/marketplace/src/instructions/listing.rs
@@ -0,0 +1,69 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{MasterEditionAccount,Metadata,MetadataAccount},
+    token::{transfer_checked,TransferChecked},
+    token_interface::{Mint,TokenAccount,TokenInterface},
+};
+use crate::state::{Listing,Marketplace};
+#[instruction(name: String)]
+#[derive(Accounts)]
+pub struct List <'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(
+        seeds =[b"marketplace",name.as_str().as_bytes()],
+        bump = marketplace.bump
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+    #[account]
+    pub maker_mint: InterfaceAccount<'info,Mint>,
+    #[account(
+        mut,
+        associated_token::mint = maker_mint,
+        associated_token::authority = maker
+    )]
+    pub maker_ata: InterfaceAccount<'info,TokenAccount>
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = maker_mint,
+        associated_token::authority = listing
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = maker,
+        seeds = [marketplace.key().as_ref(),maker_mint.key().as_ref()],
+        bump,
+        space = Listing::INIT_SPACE
+    )]
+    pub listing: Account<'info,Listing>
+    #[account]
+    pub collection_mint: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        seeds = [b"metadata",
+        metadata_program.key().as_ref(),
+        maker_mint.key().as_ref()
+        ],
+        seeds::program = metadata_program.key(),
+        bump,
+        constraint = metadata.collection.as_ref().unwrap().key().as_ref() == collection_mint.key().as_ref(),
+        constraint = 
+    )]
+    pub metadata: Account<'info,MetadataAccount>,
+    #[account(
+        seeds = [b"metadata",
+        metadata_program.key().as_ref(),
+        maker_mint.key().as_ref(),
+        b"edition"
+        
+        ]
+    )]
+    pub master_edition: Account<'info,MasterEditionAccount>,
+    pub system_program: Program<'info,System>,
+    pub token_program: Interface<'info,TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub metadata_program: Program<'info,Metadata>
+
+}
