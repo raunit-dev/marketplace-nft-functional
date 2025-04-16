f74c515ead651dd75cdf0a065826aa1fc4f43165
raunit45 <raunitjaiswal510@gmail.com>
2025-04-16 07:53:29 +0000

    wsl sucks

--- programs/marketplace/src/instructions/initialize.rs
+++ programs/marketplace/src/instructions/initialize.rs
@@ -5,9 +5,20 @@ use anchor_spl::token_interface::{Mint,TokenInterface};
 use crate::state::marketplace::Marketplace;
 
 #[derive(Accounts)]
-#[instruction(seeds: u64)]
-pub struct Initialize<`info>{
+#[instruction(name: String)]
+pub struct Initialize<'info> {
     #[account(mut)]
+    pub admin: Signer<'info>,
+    #[account(
+        init,
+        payer = admin,
+        seeds = [b"marketplace", name.as_str().as_bytes()],
+        bump,
+        space = Marketplace::INIT_SPACE
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+
+
     
 }
 
