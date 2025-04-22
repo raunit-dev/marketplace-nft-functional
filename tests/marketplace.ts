a79e9459b564ce271afbbd272b47c5640d426b90
raunit-dev <raunitjaiswal510@gmail.com>
2025-04-22 18:32:26 +0530

    testing

--- tests/marketplace.ts
+++ tests/marketplace.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import { BN, Program } from "@coral-xyz/anchor";
-import { PublicKey, SystemProgram, Keypair } from "@solana/web3.js";
+import { PublicKey, SystemProgram, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { 
   getAssociatedTokenAddressSync, 
   createMint, 
@@ -11,38 +11,68 @@ import {
 import { Marketplace } from "../target/types/marketplace";
 
 describe("marketplace", () => {
+
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-
   const program = anchor.workspace.marketplace as Program<Marketplace>;
-  const admin = provider.wallet;
+  const connection = provider.connection;
   const name = "RAUNIT";
 
+  //wallets
+  const admin = provider.wallet;
+  const maker = anchor.web3.Keypair.generate();
+  let taker =  anchor.web3.Keypair.generate();
 
+  //mints
+  let makerMint: anchor.web3.PublicKey;
+  let rewardMint: anchor.web3.PublicKey;
 
+  //atas
+  let makerAta: anchor.web3.PublicKey;
+  let takerAta: anchor.web3.PublicKey;
+  let takerRewardsAta: anchor.web3.PublicKey;
+
+  //pdas
+  let treasury: anchor.web3.PublicKey;
+  let marketplace: anchor.web3.PublicKey;
+  let vault: anchor.web3.PublicKey;
+  let listing: anchor.web3.PublicKey;
   
-  const marketplace = PublicKey.findProgramAddressSync(
+  //NFTthing
+  let collectionMint: anchor.web3.PublicKey;
+  let metedata: anchor.web3.PublicKey;
+  let masterEdition: anchor.web3.PublicKey;
+  
+
+
+  before(async() => {
+    
+    const makerAirdropSignature = await connection.requestAirdrop(maker.publicKey,10 * LAMPORTS_PER_SOL);
+    const takerAirdropSignature = await connection.requestAirdrop(taker.publicKey,10*LAMPORTS_PER_SOL);
+    const latestBlockhash = await connection.getLatestBlockhash("confirmed");
+    await connection.confirmTransaction({ signature: makerAirdropSignature, ...latestBlockhash});
+    await connection.confirmTransaction({ signature: takerAirdropSignature, ...latestBlockhash});
+    await sleep(5000)
+    
+  })
+  
+  marketplace = PublicKey.findProgramAddressSync(
     [Buffer.from("marketplace"), Buffer.from(name)],
     program.programId
   )[0];
 
 
-  const treasury = PublicKey.findProgramAddressSync(
+  treasury = PublicKey.findProgramAddressSync(
     [Buffer.from("treasury"), marketplace.toBuffer()],
     program.programId
   )[0];
 
 
-  const rewardMint = PublicKey.findProgramAddressSync(
+  rewardMint = PublicKey.findProgramAddressSync(
     [Buffer.from("rewards"), marketplace.toBuffer()],
     program.programId
   )[0];
 
-
-  console.log("Name:", name);
-  console.log("Marketplace PDA:", marketplace.toString());
-  console.log("Treasury PDA:", treasury.toString());
-  console.log("Reward Mint PDA:", rewardMint.toString());
 
   it("Marketplace Initialized", async () => {
     const tx = await program.methods.initialize(
@@ -65,4 +95,8 @@ describe("marketplace", () => {
   });
 });
 
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
